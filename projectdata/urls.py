c0dcbe39312b4417956486ffabcd84c7df46c2aa
nikhil-patil007 <nikhilpatil3701@gmail.com>
2024-01-14 01:19:03 +0530

    dev: Url Path set

--- projectdata/urls.py
+++ projectdata/urls.py
@@ -14,14 +14,35 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# from django.conf import settings
+# from django.views.static import serve
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.conf.urls import url
+
+# urlpatterns = [
+#     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+#     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+#     path('admin/', admin.site.urls),
+#     path('',include('vardhaman.urls'))
+# ]
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
-    path('',include('vardhaman.urls'))
+    path('', include('vardhaman.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# This is for serving static files in development only.
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
